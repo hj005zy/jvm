296125d0f029eb46e614c7216bb35c5ade548e79
huangjing-shanon <huangjing@shanon-cn.com>
2014-05-21 14:45:52 +0800

    init

---
+++ src/main/java/com/hacksroad/jvm/App.java
@@ -0,0 +1,13 @@
+package com.hacksroad.jvm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
